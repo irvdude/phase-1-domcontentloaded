1850f4c6fd7a5168b400cb5ba6a9f26d51d43d38
Irvin Dinkins <irvdude44@gmail.com>
2023-04-22 20:44:33 -0400

    passed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const pText = document.getElementById("text")
+    pText.textContent = "This is really cool!"
+  });
+
+
+  
